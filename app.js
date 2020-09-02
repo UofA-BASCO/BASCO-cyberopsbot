bdc26487f9b28962f31ac96bcdcf4ca3b95f88fb
Kevin Cooper <kevincooper@arizona.edu>
2020-09-02 11:19:22 -0700

    refactor command handling

--- app.js
+++ app.js
@@ -21,47 +21,54 @@ bot.on('message', msg => {
   let command = msg.content.split(' ')[0].slice(config.prefix.length)
   let args = msg.content.split(' ').slice(1)
 
-  if (command === 'ping' || command === 'pong') {
-    pingPong(command, username, msg)
+  const commands = {
+    'ping': () => pingPong(command, username, msg),
+    'addrole': () => addRole(msg, args, username),
+    'role': () => {
+      msg.reply("if you are trying to add a role, use '!addrole' with the class code behind it, e.g. '!addrole CYBV 301'. If the role for your class doesn't exist, use the command '!requestrole' using the same syntax as '!addrole'.")
+    },
+    'requestrole': () => {
+      if (!args[0]) return msg.reply('please add the role name.')
+      msg.channel.send(`Yo, <@&${748982824660369428}>, you gotta add`)
+    },
+    'removerole': () => removeRole(args, msg, username),
+    'help': () => msg.channel.send(`List of common useful commands:\n!help\n!ping\n!roles\n!role\n!addrole\n!removerole\n!requestrole\n`),
+    'args-info': () => {
+      if (!args.length) {
+        return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`)
+      }
+      msg.channel.send(`Command name: ${command}\nArguments: ${args}`)
+    },
+    'lol': () => lolCommands(command, username, msg),
   }
 
-  // Add Role Command
-  if (command === 'addrole') {
-    addRole(msg, args, username)
+  const aliases = {
+    'pong': commands.ping,
+    'roles': commands.role,
+
+    'tableflip': commands.lol,
+    'unflip': commands.lol,
+    'shrug': commands.lol,
+    'disapprove': commands.lol,
+    'lenny': commands.lol,
+    'cute': commands.lol,
+    'idk': commands.lol,
+    'awkward': commands.lol,
+    'flex': commands.lol,
+    'eat': commands.lol,
+    'food': commands.lol,
+    'sleep': commands.lol,
+    'run': commands.lol,
   }
 
-  // Roles Command
-  if (command === 'roles' || command === 'role') {
-    msg.reply("if you are trying to add a role, use '!addrole' with the class code behind it, e.g. '!addrole CYBV 301'. If the role for your class doesn't exist, use the command '!requestrole' using the same syntax as '!addrole'.")
+  const cmd = {
+    ...commands,
+    ...aliases
   }
 
-  // Request Role Command
-  if (command === 'requestrole') {
-    if (!args[0]) return msg.reply('please add the role name.')
-    msg.channel.send(`Yo, <@&${748982824660369428}>, you gotta add`)
+  if (command in cmd) {
+    return cmd[command]();
+  } else {
+    return msg.reply(`Unknown command: ${command}`)
   }
-
-  // Remove Role Command
-  if (command === 'removerole') {
-    removeRole(args, msg, username)
-  }
-
-  // Help Command
-  if (command === 'help') {
-    msg.channel.send(`List of common useful commands:\n!help\n!ping\n!roles\n!role\n!addrole\n!removerole\n!requestrole\n`)
-  }
-
-  // Args-Info Command
-  if (command === 'args-info') {
-    if (!args.length) {
-      return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`)
-    }
-
-    msg.channel.send(`Command name: ${command}\nArguments: ${args}`)
-  }
-  
-  if (command === 'tableflip' || command === 'unflip' || command === 'shrug' || command === 'disapprove' || command === 'lenny' || command === 'cute' || command === 'idk' || command === 'awkward' || command === 'flex' || command === 'eat' || command === 'food' || command === 'sleep' || command === 'run') {
-    lolCommands(command, username, msg)
-  }
-
-})
+});
