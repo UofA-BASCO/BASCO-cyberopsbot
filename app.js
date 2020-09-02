06e4a3ef4ac673b6b797426d3f215eb8976d3e48
Kevin Cooper <kevincooper@arizona.edu>
2020-09-02 14:42:19 -0700

    adhere to standards

--- app.js
+++ app.js
@@ -17,46 +17,46 @@ const lolCommands = require('./src/cmds/lolCommands')
 bot.on('message', msg => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return
 
-  let username = msg.author.username
-  let command = msg.content.split(' ')[0].slice(config.prefix.length)
-  let args = msg.content.split(' ').slice(1)
+  const username = msg.author.username
+  const command = msg.content.split(' ')[0].slice(config.prefix.length)
+  const args = msg.content.split(' ').slice(1)
 
   const commands = {
-    'ping': () => pingPong(command, username, msg),
-    'addrole': () => addRole(msg, args, username),
-    'role': () => msg.reply("if you are trying to add a role, use '!addrole' with the class code behind it, e.g. '!addrole CYBV 301'. If the role for your class doesn't exist, use the command '!requestrole' using the same syntax as '!addrole'."),
-    'requestrole': () => {
+    ping: () => pingPong(command, username, msg),
+    addrole: () => addRole(msg, args, username),
+    role: () => msg.reply("if you are trying to add a role, use '!addrole' with the class code behind it, e.g. '!addrole CYBV 301'. If the role for your class doesn't exist, use the command '!requestrole' using the same syntax as '!addrole'."),
+    requestrole: () => {
       if (!args[0]) return msg.reply('please add the role name.')
       msg.channel.send(`Yo, <@&${748982824660369428}>, you gotta add`)
     },
-    'removerole': () => removeRole(args, msg, username),
-    'help': () => msg.channel.send(`List of common useful commands:\n!help\n!ping\n!roles\n!role\n!addrole\n!removerole\n!requestrole\n`),
+    removerole: () => removeRole(args, msg, username),
+    help: () => msg.channel.send('List of common useful commands:\n!help\n!ping\n!roles\n!role\n!addrole\n!removerole\n!requestrole\n'),
     'args-info': () => {
       if (!args.length) {
         return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`)
       }
       msg.channel.send(`Command name: ${command}\nArguments: ${args}`)
     },
-    'lol': () => lolCommands(command, username, msg),
+    lol: () => lolCommands(command, username, msg)
   }
 
   const aliases = {
-    'pong': commands.ping,
-    'roles': commands.role,
+    pong: commands.ping,
+    roles: commands.role,
 
-    'tableflip': commands.lol,
-    'unflip': commands.lol,
-    'shrug': commands.lol,
-    'disapprove': commands.lol,
-    'lenny': commands.lol,
-    'cute': commands.lol,
-    'idk': commands.lol,
-    'awkward': commands.lol,
-    'flex': commands.lol,
-    'eat': commands.lol,
-    'food': commands.lol,
-    'sleep': commands.lol,
-    'run': commands.lol,
+    tableflip: commands.lol,
+    unflip: commands.lol,
+    shrug: commands.lol,
+    disapprove: commands.lol,
+    lenny: commands.lol,
+    cute: commands.lol,
+    idk: commands.lol,
+    awkward: commands.lol,
+    flex: commands.lol,
+    eat: commands.lol,
+    food: commands.lol,
+    sleep: commands.lol,
+    run: commands.lol
   }
 
   const cmd = {
@@ -65,8 +65,8 @@ bot.on('message', msg => {
   }
 
   if (command in cmd) {
-    return cmd[command]();
+    return cmd[command]()
   } else {
     return msg.reply(`Unknown command: ${command}`)
   }
-});
+})
